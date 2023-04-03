38ac3fe2391fa535bcd7b1ba2ad4b30b01ba041e
Tran Ha <hattt@caters.co.jp>
2023-04-03 21:41:18 +0700

    update

--- _develop/src/js/main.js
+++ _develop/src/js/main.js
@@ -1,10 +1,8 @@
 import 'picturefill';
 import 'smoothscroll-for-websites';
-import * as objectFitImages from 'object-fit-images';
 
 // js/tsどっちでもこんな感じで読めます
 import VhController from './utils/logic/vh-controller';
-import AnchorLink from './utils/ui/anchor-link';
 import DeviceWatcher from './utils/logic/device-watcher';
 
 import Header from './common/header';
@@ -15,11 +13,7 @@ new VhController();
 new DeviceWatcher();
 export default class Main {
   constructor() {
-    // アンカーリンク。固定ヘッダー分引くとかにも対応している。
-    // 使い方はanchor-link.ts参照。
-    new AnchorLink();
     new Header();
-    objectFitImages();
   }
 }
 
